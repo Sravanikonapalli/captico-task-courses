b0862d23c516f367a9dbd046028e3808ed3062f6
Sravanikonapalli <konapallisravani@gmail.com>
2025-01-18 08:57:26 +0530

    code

--- backend/api/server.js
+++ backend/api/server.js
@@ -2,14 +2,18 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-const authRoutes = require('./routes/auth');
-const courseRoutes = require('./routes/courses');
+const authRoutes = require('./api/routes/auth');
+const courseRoutes = require('./api/routes/courses');
 const cors = require('cors');
 
 dotenv.config();
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://captico-frontend.vercel.app', 
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));
 
 
 // Connect to MongoDB
@@ -21,6 +25,5 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
 app.use('/api/auth', authRoutes);
 app.use('/api/courses', courseRoutes);
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// Export the app for Vercel to use
+module.exports = app;
